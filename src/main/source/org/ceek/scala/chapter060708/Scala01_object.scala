fa0ead72f1e3fee7c36b3d19f15017582eddeca5
ceek <cpc_geek@163.com>
2020-10-20 16:13:07 +0800

    面向对象初探

---
+++ src/main/source/org/ceek/scala/chapter060708/Scala01_object.scala
@@ -0,0 +1,34 @@
+package org.ceek.scala.chapter060708
+
+/**
+  *
+  *
+  * @author ceek
+  * @create 2020-10-20 15:59
+  **/
+object Scala01_object {
+  def main(args: Array[String]): Unit = {
+    val user = new User();
+
+    user.username = "zhangsna";
+
+    println(user.username);
+  }
+}
+
+//声明类
+class User {
+  //类的主体内容 ==>类体
+
+  //声明属性
+  var username: String = null;
+
+  var age: Int = 0;
+
+  var score: Char = 0;
+  var score1: Byte = 0;
+
+  def log(): Boolean = {
+    true
+  }
+}
---
+++ src/main/source/org/ceek/scala/chapter060708/Scala03_package.scala
@@ -0,0 +1,11 @@
+package org.ceek.scala.chapter060708
+
+/**
+  *
+  *
+  * @author ceek
+  * @create 2020-10-20 16:11
+  **/
+object Scala03_package {
+  //
+}
