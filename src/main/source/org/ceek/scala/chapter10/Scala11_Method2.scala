e8b3e586461bf4deb4f08231ecb1c967b4684e3e
ceek <cpc_geek@163.com>
2020-11-17 17:10:05 +0800

    reduce[left/right]与fold[left/right]参数个数及含义的异同练习

--- src/main/source/org/ceek/scala/chapter10/Scala11_Method2.scala
+++ src/main/source/org/ceek/scala/chapter10/Scala11_Method2.scala
@@ -8,20 +8,24 @@ package org.ceek.scala.chapter10
   **/
 object Scala11_Method2 {
   def main(args: Array[String]): Unit = {
-    var list = List(1, 2, 3)
+    /*var list = List(1, 2, 3, 4)
+    list.reduce(_-_)
 
     //reduce方法：化简，数据减少
     //将集合的数据经过逻辑处理后只保留一个结果
-    val reducePlus: Int = list.reduce(_ - _)
-
+    val reducePlus: String = list.reduce((x, y) => y)
     val reduceLeft: Int = list.reduceLeft(_ - _)
 
     val reduceRight: Int = list.reduceRight(_ - _)
 
     //reduce和reduceLeft的执行顺序一致，均为从左到右
     //reduceRight：标识从右到左进行执行
-    println(reducePlus);
+    println(reducePlus);//reduce=reduceLeft
     println(reduceLeft);
-    println(reduceRight); //(1-(2-3))
+    println(reduceRight); //(1-(2-3))*/
+
+    var listStr = List("a", "b", "c")
+    val reduceStr: String = listStr.reduce((x, y) => y)
+    println(reduceStr)
   }
 }
--- src/main/source/org/ceek/scala/chapter10/Scala12_Method3.scala
+++ src/main/source/org/ceek/scala/chapter10/Scala12_Method3.scala
@@ -8,7 +8,14 @@ package org.ceek.scala.chapter10
   **/
 object Scala12_Method3 {
   def main(args: Array[String]): Unit = {
-    val strList = List("a", "c", "b");
+    /*val list = List(1,2,3)
+    val foldSum = list.fold(15)(_+_)
+    val foldRightSub = list.foldRight(100)(_-_);
+
+    println(foldSum)
+    println(foldRightSub) //1-(2-(3-(4-100)))*/
+
+    /*val strList = List("f", "a", "c", "b");
 
     //折叠，也可以对集合数据进行简化，获取最终的一条结果
     //fold方法可以传递2个部分的参数，
@@ -16,5 +23,18 @@ object Scala12_Method3 {
 
     //    var foldValue = list.fold("x")((x, y) => x + y);
     //    println(foldValue)
+    val right:String = strList.foldRight("p")((param, item) => item)  //返回p
+    val right2:String = strList.foldRight("p")((param, item) => param)  //返回f
+
+    println(right)*/
+
+    //TODO scan 会将第一个参数保留输出
+    val list = List(1, 2, 5)
+    val scan: List[Int] = list.scan(10)(_ - _)
+    val scanLeft: List[Int] = list.scanLeft(10)(_ - _)
+    val scanRight: List[Int] = list.scanRight(10)(_ - _) //1 - (2 - (5-10))
+    println(scan)
+    println(scanLeft)
+    println(scanRight)
   }
 }
